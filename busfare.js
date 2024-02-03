c3c0478b744f7263d73424f19f2c125c5bbc7d1c
shohag0007 <merinedrive007@gmail.com>
2024-02-03 21:32:25 +0600

    about js-conditionals

---
+++ busfare.js
@@ -0,0 +1,18 @@
+
+ const fare = 800;
+ const age = 10;
+ 
+ 
+
+   if (age <= 10 ) {
+      console.log('travel free');
+   }
+
+ else if(age >= 60){
+   console.log('You can get 15% discount');
+ }
+
+
+ 
+
+ 
